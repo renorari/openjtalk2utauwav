6b00a36b74794a4918611299592c5c756a5d8f6c
TarochanChannel <gakutyn.2020@gmail.com>
2022-01-30 21:39:18 +0900

    ちょっと改良

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ const oto_list = fs.readdirSync("C:/Program Files (x86)/UTAU/voice/uta").filter(
 
 oto_list.forEach((oto, index) => {
     console.log(`processing: ${Math.floor(index / oto_list.length * 100)}% ${oto}`);
-    child_process.execSync(`echo ${JSON.stringify(oto.replace(/.wav/, "") + "ー")} | open_jtalk -x C:/open_jtalk/dic -m voice.htsvoice -ow wav/${oto}`);
+    child_process.execSync(`echo ${JSON.stringify(oto.replace(/.wav/, "") + "ーー")} | open_jtalk -x C:/open_jtalk/dic -m voice.htsvoice -ow wav/${oto}`);
 });
